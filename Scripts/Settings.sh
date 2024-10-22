05d1498076f560c8ecf36ad125a0f92af62322fd
SingTsang <SingTsang@users.noreply.github.com>
2024-10-22 08:37:30 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -45,6 +45,8 @@ if [[ $WRT_BRANCH == *"23.05"* ]]; then
 
 	echo "CONFIG_PACKAGE_luci-app-homeproxy=y" >> ./.config
 	echo "CONFIG_PACKAGE_luci-app-upnp-mtk-adjust=y" >> ./.config
+ 
+ 	echo "CONFIG_PACKAGE_luci-proto-bonding=y" >> ./.config
 	echo "CONFIG_PACKAGE_luci-proto-wireguard=y" >> ./.config
 	echo "CONFIG_PACKAGE_luci-app-ddns=y" >> ./.config
 	echo "CONFIG_PACKAGE_luci-app-dockerman=y" >> ./.config
